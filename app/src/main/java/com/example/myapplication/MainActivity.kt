d7999f24d6d27a81075274a82a48ae0c36282f0b
dennalig <dennaligrissom7@gmail.com>
2020-12-30 11:57:26 -0500

    ...

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var enterView = findViewById<TextView>(R.id.enterButton)
     }
 }
